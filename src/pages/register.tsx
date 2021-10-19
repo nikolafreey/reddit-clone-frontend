50da8439522398211f1bcaca80fe9cf3de7e7cbd
Nikola Vukovic <nikolav54@gmail.com>
2021-10-19 23:30:19 +0200

    Added error handling on the front and fixed bugs in register mutation

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -5,17 +5,26 @@ import { Button } from "@chakra-ui/button";
 import Wrapper from "../components/Wrapper";
 import InputField from "../components/InputField";
 import { useRegisterMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
+  const router = useRouter();
   const [, register] = useRegisterMutation();
   return (
     <Wrapper variant="regular">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={async (values) => {
+        onSubmit={async (values, { setErrors }) => {
           const response = await register(values);
+          if (response.data?.register.errors) {
+            setErrors(toErrorMap(response.data.register.errors));
+          } else if (response.data?.register.user) {
+            console.log("router.push();");
+            router.push("/");
+          }
         }}
       >
         {({ isSubmitting }) => (
